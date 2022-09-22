9e95294ccbac72e495be6e692d2b47ed71b76c0f
natsutan <YRW03704@nifty.com>
2022-09-22 20:39:30 +0900

    step1終了

--- r9cc/src/main.rs
+++ r9cc/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::env;
+use std::path::Path;
+use std::fs::File;
+use std::process;
+use std::error::Error;
+use std::io::prelude::*;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args :Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage r9cc n");
+        process::exit(1);
+    }
+
+    let path = Path::new("tmp.s");
+
+    let mut file = File::create(path)?;
+    writeln!(file, ".intel_syntax noprefix")?;
+    writeln!(file, ".globl main")?;
+    writeln!(file, "main:")?;
+    writeln!(file, "  mov rax, {}", args[1])?;
+    writeln!(file, "  ret")?;
+
+    Ok(())
 }
